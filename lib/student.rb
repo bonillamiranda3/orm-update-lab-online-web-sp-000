53ea66b28663671f0629f5c120342c0e1eb6a9d3
Miranda Bonilla <bonillamiranda3@gmail.com>
2019-05-28 01:49:07 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -60,11 +60,11 @@ class Student
       self.new_from_db(student[0])
     end
 
-      def update
-        sql = <<-SQL
-        UPDATE students SET name = ?. grade = ?, WHERE id =?
-        SQL
-        DB[:conn].execute(sql, self.name, self.id, self.grade)
-      end
+    def update
+    sql = <<-SQL
+      UPDATE students SET name = ?, grade = ? WHERE id = ?
+    SQL
+    DB[:conn].execute(sql, self.name, self.grade, self.id)
+  end
 
 end
